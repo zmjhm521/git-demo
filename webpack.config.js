e90b473c2ccd61a2a0d2a9044c8fffe5dac9b1fb
zhaomingjie <1441586363@qq.com>
2022-12-09 16:53:40 +0800

    entry

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: "production",
+    // entry:"./hello/hello.js",//指定打包时的主文件 默认 ./src/index.js
+    // entry:["./src/a.js","./src/b.js"],//数组合并，对象分开
+    entry:{
+        a:"./src/a.js",
+        b:"./src/b.js",
+    }
+}
