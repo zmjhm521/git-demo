1fa7b3a40706d4d472ecd7fd9e6ef2f07818bef7
zhaomingjie <1441586363@qq.com>
2022-12-09 17:14:24 +0800

    enty

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     mode: "production",
     // entry:"./hello/hello.js",//指定打包时的主文件 默认 ./src/index.js
-    // entry:["./src/a.js","./src/b.js"],//数组合并，对象分开
-    entry:{
-        a:"./src/a.js",
-        b:"./src/b.js",
-    }
+    entry:["./src/a.js","./src/b.js"],//数组合并，对象分开
+    // entry:{
+    //     a:"./src/a.js",
+    //     b:"./src/b.js",
+    // }
+
 }
